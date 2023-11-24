9d16739b493ba0180aacd880b40137b87a882a84
Davi Borges <davifelipe_300@hotmail.com>
2023-11-24 00:22:41 -0300

    altera tipo prop lista atividades

--- src/app/views/medicos/models/visualizar-medico.view-model.ts
+++ src/app/views/medicos/models/visualizar-medico.view-model.ts
@@ -1,16 +1,8 @@
+import { ListarAtividadeViewModel } from '../../atividades/models/listar-atividades.view-model'
 export type VisualizarMedicoViewModel = {
   id: string;
   nome: string;
   especialidade: string;
   crm: string;
-  listaAtividades: ListarAtividadesViewModel[];
+  listaAtividades: ListarAtividadeViewModel[];
 };
-
-export type ListarAtividadesViewModel = {
-  id: string;
-  paciente: string;
-  data: string;
-  horarioTermino: string;
-  horarioInicio: string;
-  tipoAtividade: number;
-}
